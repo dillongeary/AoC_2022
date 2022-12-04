8c73bfa09fedc1683f9279b5aa2e5bed411a6105
Dillon <dgfg1g20@soton.ac.uk>
2022-12-04 13:45:23 +0000

    day 4 done

---
+++ 4b.hs
@@ -0,0 +1,27 @@
+import Data.List
+
+main = do lines <- getLines
+          putStrLn $ show $ length $ filter (filterFunc) $ map (format . splitFunc) lines
+
+getLines :: IO ([String])
+getLines = do x <- getLine
+              if x == ""
+                 then return []
+                 else do xs <- getLines
+                         return (x:xs)
+
+format :: [String] -> ((Int,Int),(Int,Int))
+format [a,b,c,d] = ((read a, read b),(read c,read d))
+
+filterFunc :: ((Int,Int),(Int,Int)) -> Bool
+filterFunc ((a,b),(c,d)) | b < c     = False
+                         | a > d     = False
+                         | otherwise = True
+
+splitFunc :: String -> [String]
+splitFunc xs = splitFunc' xs []
+  where splitFunc' :: String -> String -> [String]
+        splitFunc' [] as = [as]
+        splitFunc' ('-':xs) as = as : splitFunc' xs []
+        splitFunc' (',':xs) as = as : splitFunc' xs []
+        splitFunc' (x:xs) as = splitFunc' xs (as ++ [x])
