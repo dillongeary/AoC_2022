251db454c523fa28ff3064c8554dbb1192488883
Dillon <dgfg1g20@soton.ac.uk>
2022-12-07 12:50:31 +0000

    generalised solution to be consistence between both 6a and 6b

--- 6a.hs
+++ 6a.hs
@@ -1,9 +1,9 @@
 import Data.List
 
 main = do line <- getLine
-          putStrLn $ show $ counter line 4
+          putStrLn $ show $ counter line 4 4
 
-counter :: String -> Int -> Int
-counter (a:b:c:d:es) n | areUnique = n
-                       | otherwise = counter (b:c:d:es) (n+1)
-  where areUnique = (length $ group $ sort [a,b,c,d]) == 4
+counter :: String -> Int -> Int -> Int
+counter xs n m | areUnique = n
+               | otherwise = counter (tail xs) (n+1)
+  where areUnique = (length $ group $ sort $ take m xs) == m
--- 6b.hs
+++ 6b.hs
@@ -1,9 +1,9 @@
 import Data.List
 
 main = do line <- getLine
-          putStrLn $ show $ counter line 14
+          putStrLn $ show $ counter line 14 14
 
-counter :: String -> Int -> Int
-counter xs n | areUnique = n
-             | otherwise = counter (tail xs) (n+1)
-  where areUnique = (length $ group $ sort $ take 14 xs) == 14
+counter :: String -> Int -> Int -> Int
+counter xs n m | areUnique = n
+               | otherwise = counter (tail xs) (n+1)
+  where areUnique = (length $ group $ sort $ take m xs) == m
